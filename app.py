1288512427bcaf3bd1880f0abd8b48b00ca8b4a6
ilookyou <101230915+ilookyou@users.noreply.github.com>
2022-03-21 14:48:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -7,11 +8,15 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+# -----------------------------------------------------------------------------------------------
 app = Flask(__name__)
-
+# -----------------------------------------------------------------------------------------------
 line_bot_api = LineBotApi(
     'f64+DlAAXN5OOSqCFMTnTIZm7uP9ZF0JIV2etGu5iFcWcAnCs16wtZoWTnkaa0/1UOfnfFRnhPIZfay5QXOh7e4IWJK7wqO+0/B8SUfL+2PPoxMoYFwD3YoKdFU93pCQ9GJgvvBWmsdXF4R9dC+vsgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('434126c11cd8861ccc01da4fdcb978b6')
+
+
+# -----------------------------------------------------------------------------------------------
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -30,17 +35,12 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_text = event.message.text
-    if line_text == 'test':
+    if line_text =='test':
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='伺服器連線正常'))
-
-
-
-
-
-
 
 
 
